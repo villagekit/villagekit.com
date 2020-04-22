cae12da94c07764977b1e43c739f43e93173f244
Michael Williams <michael.williams@enspiral.com>
2020-04-22 17:02:11 +1200

    basic website taken from c-prize 2019 entry content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,25 @@
 module.exports = {
   siteMetadata: {
     title: `Village Kit`,
-    description: `Speed up your GatsbyJS development workflow. Designed as a set of opinionated and advanced themes and starters using MDX and Theme-UI. Incorporates gatsby-theme-catalyst-core, gatsby-theme-catalyst-header-top, and gatsby-theme-catalyst-footer.`,
-    keywords: [`gatsby`, `theme`, `react`],
-    author: `Eric Howey`,
-    siteUrl: `https://gatsby-starter-catalyst.netlify.app`, // Change to you site address, required for sitemap.xml and robots.txt file among other things
+    description: `Village Kit is a platform for open source, modular living infrastructure that evolves with you.`,
+    keywords: [
+      `modular`,
+      `open`,
+      `furniture`,
+      `construction`,
+      `village`,
+      `kit`,
+      `catalog`,
+    ],
+    author: `Village Kit Limited`,
+    siteUrl: `https://villagekit.com`,
     menuLinks: [
-      /*
       {
-        name: `Page 1`,
-        link: `/page-1`,
-        type: `internal`, //internal or anchor
+        name: `Team`,
+        link: `/team`,
+        type: `internal`,
       },
+      /*
       {
         name: `Page 2`,
         link: `/page-2`,
@@ -77,18 +85,18 @@ module.exports = {
         // footerContentLocation: "left", // "left", "right", "center"
       },
     },
-    `gatsby-theme-catalyst-header-top`, // Try `gatsby-theme-catalyst-header-side`
+    `gatsby-theme-catalyst-header-top`,
     `gatsby-theme-catalyst-footer`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-catalyst`,
-        short_name: `catalyst`,
+        name: `villagekit-com`,
+        short_name: `villagekit`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#cccccc`,
         display: `minimal-ui`,
-        icon: `content/assets/catalyst-site-icon.png`, // This path is relative to the root of the site.
+        icon: `content/assets/catalyst-site-icon.png`,
       },
     },
     `gatsby-plugin-netlify`,
