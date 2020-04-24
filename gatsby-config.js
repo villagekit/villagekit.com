2aa8206826eb41fed24cdf95df7947439b824048
Mikey <michael.williams@enspiral.com>
2020-04-24 14:05:01 +1200

    blog (#1)
    * basic catalyst blog
    * produce rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,11 @@ module.exports = {
         link: `/team`,
         type: `internal`,
       },
+      {
+        name: `Blog`,
+        link: `/blog`,
+        type: `internal`,
+      },
       /*
       {
         name: `Page 2`,
@@ -97,6 +102,77 @@ module.exports = {
         theme_color: `#cccccc`,
         display: `minimal-ui`,
         icon: `content/assets/catalyst-site-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-theme-catalyst-blog`,
+      options: {
+        excerptLength: `280`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            output: `/rss.xml`,
+            title: `Village Kit RSS Feed`,
+            query: `
+              {
+                allCatalystPost(
+                  sort: { fields: [date, title], order: DESC }
+                  limit: 1000
+                  filter: { draft: { eq: false } }
+                ) {
+                  nodes {
+                    id
+                    slug
+                    title
+                    author
+                    excerpt
+                    date(formatString: "ddd, DD MMM YYYY HH:mm:ss ZZ")
+                    featuredImage {
+                      childImageSharp {
+                        resized:resize(width: 1024) {
+                          src
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            serialize: ({
+              query: {
+                site: {
+                  siteMetadata: { siteUrl },
+                },
+                allCatalystPost,
+              },
+            }) => {
+              return allCatalystPost.nodes.map((node) => ({
+                guid: `${siteUrl}/id/${node.id}`,
+                link: `${siteUrl}${node.slug}`,
+                title: node.title,
+                author: node.author,
+                description: node.excerpt,
+                pubDate: node.date,
+                image: `${siteUrl}${node.featuredImage.childImageSharp.resized.src}`,
+              }))
+            },
+          },
+        ],
       },
     },
     `gatsby-plugin-netlify`,
