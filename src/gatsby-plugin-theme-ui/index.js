889a0dec000ace1b96cda737ed51d2594381e2ac
Michael Williams <michael.williams@enspiral.com>
2020-05-16 12:07:05 +1200

    increase max content width to 1440px

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -4,6 +4,10 @@ import { BaseTheme } from 'gatsby-theme-catalyst-core'
 // https://www.gatsbycatalyst.com/docs/theme-ui
 
 export default merge(BaseTheme, {
+  sizes: {
+    ...BaseTheme.sizes,
+    maxContentWidth: '1440px',
+  },
   variants: {
     siteTitle: {
       fontSize: [3, 4, null, 5, null],
